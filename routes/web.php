c6e1b4ccdffd1aea4d7de20fa4d347387176f676
Mubeen Ameeen <mubeen425@gmail.com>
2020-12-09 23:39:40 +0500

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::resource('user', 'UserController');
+
+    Route::resource('permission', 'PermissionController');
+
+    Route::resource('role', 'RoleController');
+
+
+
+    Route::get('/profile', 'UserController@profile')->name('user.profile');
+
+    Route::post('/profile', 'UserController@postProfile')->name('user.postProfile');
+
+    Route::get('/password/change', 'UserController@getPassword')->name('userGetPassword');
+
+    Route::post('/password/change', 'UserController@postPassword')->name('userPostPassword');
+});
+
+
+
+
+
+Auth::routes();
+
+
+//////////////////////////////// axios request
+
+Route::get('/getAllPermission', 'PermissionController@getAllPermissions');
+Route::post("/postRole", "RoleController@store");
+Route::get("/getAllUsers", "UserController@getAll");
+Route::get("/getAllRoles", "RoleController@getAll");
+Route::get("/getAllPermissions", "PermissionController@getAll");
+
+/////////////axios create user
+Route::post('/account/create', 'UserController@store');
